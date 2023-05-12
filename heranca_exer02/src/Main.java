db88da288622e3b4f5090b24f073a0adc7c945a2
moacirheck1 <moacir-heck@educar.rs.gov.br>
2023-05-11 22:08:32 -0300

    enviando projeto

--- heranca_exer02/src/Main.java
+++ heranca_exer02/src/Main.java
@@ -4,9 +4,26 @@ public class Main {
     public static void main(String[] args) {
 
         Veiculo pampa = new Carro(2, "Ford", "Pampa", 1990);
+        Veiculo gol = new Carro(2, "vw", "gol", 1990);
+        Veiculo fusca = new Carro(2, "vw", "fusca", 2000);
+        Veiculo opala = new Carro(2, "chevrolet", "Pampa", 1991);
+        Veiculo chevette = new Carro(2, "chevrolet", "chevette", 1992);
+        Veiculo cg150 = new Moto(true, "Honda", "Cg 150", 2006);
+        Veiculo bros = new Moto(true, "Honda", "Bros", 2007);
+        Veiculo bis = new Moto(true, "Honda", "bis 125", 2020);
         Veiculo cg125 = new Moto(true, "Honda", "Cg 125", 2000);
+        Veiculo pop = new Moto(true, "Honda", "pop 100", 2018);
         System.out.println(pampa.exibeDetalhes());
+        System.out.println(gol.exibeDetalhes());
+        System.out.println(fusca.exibeDetalhes());
+        System.out.println(opala.exibeDetalhes());
+        System.out.println(chevette.exibeDetalhes());
+        System.out.println(cg150.exibeDetalhes());
+        System.out.println(bros.exibeDetalhes());
+        System.out.println(bis.exibeDetalhes());
         System.out.println(cg125.exibeDetalhes());
+        System.out.println(pop.exibeDetalhes());
+     
 
     }
 }
